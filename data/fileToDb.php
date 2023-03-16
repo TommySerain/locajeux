2d513b5e616623b3db9e4bd585cbb5851aa10964
TommySerain <tommyserain@yahoo.fr>
2023-03-16 22:05:47 +0100

    passge d'une requete query vers une requete préparé pour l'upload du .txt vers mysql

--- data/fileToDb.php
+++ data/fileToDb.php
@@ -15,10 +15,25 @@ try {
         $locGame=$game->getLocP();
         $cautionGame=$game->getCautP();
         
-        $statement=$pdo->query("INSERT INTO jeux (id_j,name_j, img_j, rules_j, loc_j, caution_j, id_t, id_c)
-        VALUES ('$idGame','$nameGame','$PictureGame','$rulesGame',$locGame,$cautionGame,$typeGame,$catGame)");
+        $statement=$pdo->prepare("INSERT INTO jeux (id_j,name_j, img_j, rules_j, loc_j, caution_j, id_t, id_c)
+        VALUES (:id,:name,:img,:rules,:loc,:caution,:id_t,:id_c)");
+        $statement->execute(
+            [
+                'id'=>$idGame,
+                'name'=>$nameGame,
+                'img'=>$PictureGame,
+                'rules'=>$rulesGame,
+                'loc'=>$locGame,
+                'caution'=>$cautionGame,
+                'id_t'=>$typeGame,
+                'id_c'=>$catGame,
+            ]
+        );
         $stmt= $pdo->query("SELECT * FROM jeux");
     };
 } catch (PDOException $e) {
     
 }
+// $statement=$pdo->query("INSERT INTO jeux (id_j,name_j, img_j, rules_j, loc_j, caution_j, id_t, id_c)
+//         VALUES ('$idGame','$nameGame','$PictureGame','$rulesGame',$locGame,$cautionGame,$typeGame,$catGame)");
+//         $stmt= $pdo->query("SELECT * FROM jeux");
