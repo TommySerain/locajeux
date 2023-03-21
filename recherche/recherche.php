efe032e03c3e4ee852ec0cdf923cdeb1adda30de
TommySerain <tommyserain@yahoo.fr>
2023-03-21 10:27:27 +0100

    ameliration_recherche_suppression_elseif

--- recherche/recherche.php
+++ recherche/recherche.php
@@ -1,57 +1,33 @@
 <?php
 
+$joinRequest=[];
+$whereRequest=[];
 if(count($_GET)!==0){
     $type=$_GET['type'];
     $cat=$_GET['categories'];
     $name=$_GET['nom'];
+
+    if(!empty($type)){
+        $joinRequest[]="NATURAL JOIN types";
+        $whereRequest[]="name_t='$type'";
+    }
+    if(!empty($cat)){
+        $joinRequest[]="NATURAL JOIN categories";
+        $whereRequest[]="name_c='$cat'";
+    }
+    if(!empty($name)){
+        $whereRequest[]="name_j LIKE '%$name%'";
+    }
+    $where=implode(' AND ', $whereRequest);
+    $join=implode(' ', $joinRequest);
+    var_dump($whereRequest);
+    $request="SELECT * FROM jeux $join WHERE $where;";
+    var_dump($request);
+    $stmt=$pdo->query($request);
 }
 
-
-if (empty($_GET)){
-    while ($game = $stmt->fetch()){?>
-        <div class="col-3 p-0">
-            <div class="rounded-4 m-4 jeux">
-            <a href="fichejeux.php?id=<?php echo $game['id_j'];?>">
-                <img class="imgJeux w-100 m-0 border border-4  border-dark rounded-4  jeux" src="<?php echo $game['img_j'] ;?>" alt="">
-            </a>
-            </div>
-        </div>
-    <?php
-    }
-}else if (empty($_GET['type']) && empty($_GET['categories'])){
-    $stmt= $pdo->prepare("SELECT * FROM jeux WHERE name_j LIKE :nom ;");
-    $stmt->execute([
-        'nom'=>'%'.$name.'%',
-    ]);
-}else if (empty($_GET['type']) && empty($_GET['nom'])){
-    $stmt= $pdo->prepare("SELECT * FROM jeux NATURAL JOIN categories WHERE name_c = :cat ;");
-    $stmt->execute([
-        'cat'=>$cat,
-    ]);
-}else if (empty($_GET['categories']) && empty($_GET['nom'])){
-    $stmt= $pdo->prepare("SELECT * FROM jeux NATURAL JOIN types WHERE name_t = :types ;");
-    $stmt->execute([
-        'types'=>$type,
-    ]);
-}else if (empty($_GET['type'])){
-    $stmt= $pdo->prepare("SELECT * FROM jeux NATURAL JOIN categories WHERE name_c = :cat AND name_j LIKE :nom ;");
-    $stmt->execute([
-        'cat'=>$cat,
-        'nom'=>'%'.$name.'%',
-    ]);
-}else if (empty($_GET['categories'])){
-    $stmt= $pdo->prepare("SELECT * FROM jeux NATURAL JOIN types WHERE name_t = :types AND name_j LIKE :nom ;");
-    $stmt->execute([
-        'types'=>$type,
-        'nom'=>'%'.$name.'%',
-    ]);
-}else if (empty($_GET['nom'])){
-    $stmt= $pdo->prepare("SELECT * FROM jeux NATURAL JOIN types NATURAL JOIN categories WHERE name_t = :types AND name_c = :cat ;");
-    $stmt->execute([
-        'types'=>$type,
-        'cat'=>$cat,
-    ]);
-}else{
+/*
+else{
     $stmt= $pdo->prepare("SELECT * FROM jeux NATURAL JOIN types NATURAL JOIN categories WHERE name_t=:types AND name_c=:cat AND name_j LIKE :nom ;");
     $stmt->execute([
         'types'=>$type,
@@ -59,7 +35,7 @@ if (empty($_GET)){
         'nom'=>'%'.$name.'%',
     ]);
 }
-
+*/
     while ($game = $stmt->fetch()){
         ?>
         <div class="col-3 p-0">
